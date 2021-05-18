ef3aae95da5739b61aa2431de3e2dac165fc1c99
francosauvisky <francosauvisky@gmail.com>
2021-05-18 11:52:41 -0300

    context & program creation code

--- src/oclsim.c
+++ src/oclsim.c
@@ -13,74 +13,110 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
 
-typedef struct _oclsim_context
+#define PINFORM(x, ...) {fprintf(stderr, (x), ##__VA_ARGS__)}
+#define PERROR(x) {fprintf(stderr,\
+"Error on line %d, file %s (function %s):\n%s",\
+__LINE__, __FILE__, __func__, (x))}
+#define CHKERROR(flag,str) {if(flag){PERROR(str),exit(1)}}
+
+struct oclsim_con
 {
   cl_platform_id platform;
   cl_device_id device;
   cl_context context;
-} oclCon_s;
+  cl_command_queue queue;
+};
 
-typedef struct _oclsim_system
+struct oclsim_sys
 {
-  cl_program *program;
-  cl_command_queue *queue;
+  cl_program program;
   cl_kernel *kernels;
   cl_mem *buffers;
   cl_event *events;
-} oclSys_s;
+};
 
 oclCon
 oclsim_init(int plati, int devi)
 {
-  oclCon newcon = malloc(sizeof(oclCon_s));
+  oclCon newcon = malloc(sizeof(struct oclsim_con));
   cl_int err=0;
-
   size_t plats_n;
+
   err = clGetPlatformIDs(0,NULL,&plats_n);
   cl_platform_id platforms[plats_n];
-  CHKERROR(err,"Couldn't get platforms number");
-  CHKERROR(-((plati<0)||(plati>=plats_n)), "Selected platform is out of range");
+  CHKERROR((plati<0)||(plati>=plats_n), "Selected platform is out of range");
 
 	err = clGetPlatformIDs(plats_n, platforms, NULL);
-  CHKERROR(err,"Couldn't idenfity platforms");
+  CHKERROR(err<0,"Couldn't idenfity platforms");
 
   newcon->platform = platforms[plati];
 
   char platname[100];
   err = clGetPlatformInfo(newcon->platform,CL_PLATFORM_NAME,100,platname,NULL);
-  CHKERROR(err,"Couldn't get platform name");
+  CHKERROR(err<0,"Couldn't get platform name");
   PINFORM("Selected platform: %s", platname);
 
   size_t devs_n;
   err=clGetDeviceIDs(newcon->platform,CL_DEVICE_TYPE_ALL,0,NULL,&devs_n);
+
   cl_device_id devices[devs_n];
   err=clGetDeviceIDs(newcon->platform,CL_DEVICE_TYPE_ALL,devs_n,devices,NULL);
-  CHKERROR(err,"Couldn't access devices");
+  CHKERROR(err<0,"Couldn't identify device");
+  CHKERROR((plati<0)||(plati>=plats_n),"Selected platform is out of range");
 
   newcon->device = devices[devi];
-
-  // // GPU
-  // err = clGetDeviceIDs(newcon->platform, CL_DEVICE_TYPE_GPU, 1,
-  //                      &newcon->device, NULL);
-  // if(err == CL_DEVICE_NOT_FOUND)
-  // {
-  //   // CPU
-  //   PINFORM(stderr,"USING CPU!\n");
-  //   err = clGetDeviceIDs(newcon->platform, CL_DEVICE_TYPE_CPU, 1,
-  //                        &newcon->device, NULL);
-  // }
-  // CHKERROR(err,"Couldn't access devices");
-
   newcon->context = clCreateContext(NULL, 1, &newcon->device, NULL, NULL, &err);
-  CHKERROR(err,"Couldn't create context");
+  CHKERROR(err<0,"Couldn't create context");
+
+  newcon->queue = clCreateCommandQueueWithProperties(context, device,
+                  (const cl_queue_properties[])
+                  {CL_QUEUE_PROPERTIES,CL_QUEUE_PROFILING_ENABLE, 0}, &err);
 
   return newcon;
 }
 
-int
-oclsim_new_sys(oclCon con, char *src_file, oclSys *sys)
+oclSys
+oclsim_new_sys(oclCon con, char *src_file)
 {
-    oclSys_s new_sys = malloc(sizeof(oclSys_s));
+  oclSys new_sys = malloc(sizeof(struct oclsim_sys));
+
+  // copy file to buffer
+  FILE *src_fh = fopen(src_file, "r");
+  char *src_buff;
+  size_t src_size;
+  cl_int err=0;
+  CHKERROR(src_fh==NULL, "Couldn't open .cl src file");
+
+  fseek(src_fh, 0, SEEK_END);
+  src_size = ftell(program_handle);
+  rewind(src_fh);
+
+  src_buff = (char*)malloc(src_size + 1);
+  src_buff[src_size] = '\0';
+  fread(src_buff, sizeof(char), src_size, src_fh);
+  fclose(src_fh);
+
+  new_sys->program = clCreateProgramWithSource(ctx, 1,(const char**)&src_buff,
+                                               &src_size, &err);
+  CHKERROR(err<0, "Couldn't create program");
+  free(src_buff);
+
+  err = clBuildProgram(new_sys->program, 0, NULL, "-I.", NULL, NULL);
+  if(err < 0)
+  {
+    size_t log_size;
+    clGetProgramBuildInfo(new_sys->program, new_sys->device,
+                         CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+    char *log_buff = (char*) malloc(log_size + 1);
+    log_buff[log_size] = '\0';
+    clGetProgramBuildInfo(new_sys->program, new_sys->device,
+                          CL_PROGRAM_BUILD_LOG, log_size + 1, log_buff, NULL);
+    printf("%s\n", log_buff);
+    free(log_buff);
+    exit(1);
+  }
+
+  return new_sys;
 }
 
 int
@@ -98,6 +134,7 @@ oclsim_get_data(oclSys sys, void *dataptr)
 int
 oclsim_destroy_con(olcCon con)
 {
+  clReleaseContext(con->context);
   free(con);
 }
 
